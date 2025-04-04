0d0f0cf4f12d6157a5487e936cdcdea345e90186
Dylan Sprague <dylan.richard.sprague@gmail.com>
2025-04-04 13:50:01 -0400

    Install tsx, use Node's built-in test runner for tests

--- test/exampleValue.test.ts
+++ test/exampleValue.test.ts
@@ -1,7 +1,10 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 import { testValue } from "../src/exampleValue";
+import { describe, it } from "node:test";
+import assert from "node:assert/strict";
 
 describe("Main tests", () => {
   it("Expects that 1 = 1", () => {
-    expect(testValue).toBe(1);
+    assert.equal(testValue, 1);
   });
 });
